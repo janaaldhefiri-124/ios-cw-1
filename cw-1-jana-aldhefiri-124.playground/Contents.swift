f96ccf1a56e7e720822cb07943f45dd3f8d0e20d
جنى مسيلم الظفيري124 <janamusailem2@gmail.com>
2020-06-14 14:17:52 +0300

    done

---
+++ cw-1-jana-aldhefiri-124.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//part one
+let name : String = "jana"
+var hobby1 = "volunteering"
+var hobby2 = "reading"
+var age = 16
+var hieght = 1.59
+
+print("hello my name is:" ,name, "i enjoy" ,hobby1 ,"and", hobby2,"i am",age,hieght)
+
+
+//part two
+let FriendName = "rana"
+var FriendHieght = 1.58
+var FriendAge = 16
+var FriendHobbey1 = "eating"
+var FriendHobbey2 = "swimming"
+
+ //comparing
+
+name != FriendName
+
+age == FriendAge
+age > FriendAge
+age < FriendAge
+age >= FriendAge
+age <= FriendAge
+
+hieght != FriendHieght
+hobby1 != FriendHobbey1
+hobby2 != FriendHobbey2
+hobby1 == FriendHobbey1
+hobby2 == FriendHobbey2
+
+hieght == FriendHieght
+hieght != FriendHieght
+hieght < FriendHieght
+hieght > FriendHieght
+
+
+let bestfriends =
+    hobby1 == FriendHobbey1 ||
+    hobby1 == FriendHobbey2
+
+
+
